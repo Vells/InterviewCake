ac94fca916afc8273c7fba21514bd5a6dd46aa5c
Vells <vdmineva@gmail.com>
2016-07-26 19:06:46 -0700

    Fixed a bug in Problem 30

--- 30_permutation_palindrome.py
+++ 30_permutation_palindrome.py
@@ -5,6 +5,9 @@ import json
 """
 Problem #30
 
+Write an efficient function that checks whether
+any permutation of an input string is a palindrome.
+
 For a string of length n, there are n! permutations
 (n choices for the first character, times n−1 choices for the second character, etc).
 Checking each length-n permutation to see if it's a palindrome involves
@@ -23,13 +26,19 @@ def permutation_palindrome_1(word):
 
     odd_counts = 0
     for count in letter_counts.values():
-        odd_counts += 1 if not count % 2 else 0
+        odd_counts += 1 if count % 2 else 0
 
     print "letter_counts: {0}".format(json.dumps(letter_counts, indent=4))
-    return not odd_counts % 2
+    return odd_counts == 1
 
 
 def permutation_palindrome(word):
+    """
+    Time complexity: O(n) where n is the length of the word
+    Space complexity: O(n) - size of the set unpaired_chars
+        but since there is a limited number of unique characters in both the
+        Unicode and ASCII char sets, we might want to say that space complexity is O(1)
+    """
     unpaired_characters = set()
 
     for letter in word:
@@ -41,7 +50,7 @@ def permutation_palindrome(word):
     return len(unpaired_characters) <= 1
 
 
-assert permutation_palindrome("civic") is True
-assert permutation_palindrome("ivicc") is True
-assert permutation_palindrome("civil") is False
-assert permutation_palindrome("livci") is False
+assert permutation_palindrome_1("civic") is True
+assert permutation_palindrome_1("ivicc") is True
+assert permutation_palindrome_1("civil") is False
+assert permutation_palindrome_1("livci") is False
